84e313a2c4ec8204d8e034afc806c6adb20d313f
yuranous <garkavy_yuri@mail.ru>
2021-07-17 08:02:34 +0300

    Added default PORT variable value for docker-compose.yml Added default values for data related variables, removed raising exceptions Modified README.md due to other code changes

--- app/settings.py
+++ app/settings.py
@@ -4,10 +4,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DEFAULT_FILE_PATH = './signatures.tsv'
+DEFAULT_FILE_SEPARATOR = '\t'
+
 FILE_PATH = os.environ.get('FILE_PATH', '')
 if not FILE_PATH:
-    raise ValueError('FILE_PATH environment variable value must be provided')
+    FILE_PATH = DEFAULT_FILE_PATH
 
 FILE_SEPARATOR = os.environ.get('FILE_SEPARATOR', '')
 if not FILE_SEPARATOR:
-    raise ValueError('FILE_SEPARATOR environment variable value must be provided')
+    FILE_SEPARATOR = DEFAULT_FILE_SEPARATOR
